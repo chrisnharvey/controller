50bd49b6070e23aff4ba3b26e69c4fdaa2d31f05
Chris Harvey <chris@chrisnharvey.com>
2014-04-03 22:42:32 +0100

    Implementing ArrayAccess

--- src/Controller.php
+++ src/Controller.php
@@ -6,7 +6,7 @@ use Encore\View\View;
 use Encore\Container\ContainerAwareTrait;
 use Encore\Container\ContainerAwareInterface;
 
-abstract class Controller implements ContainerAwareInterface
+abstract class Controller implements ContainerAwareInterface, \ArrayAccess
 {
     use ContainerAwareTrait;
 
@@ -20,5 +20,50 @@ abstract class Controller implements ContainerAwareInterface
         $view->setController($this);
 
         $this->view = $view;
+    }
+
+    /**
+     * Determine if a given offset exists.
+     *
+     * @param string $key
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->container[$offset]);
+    }
+
+    /**
+     * Get the value at a given offset.
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return $this->container[$offset];
+    }
+
+    /**
+     * Set the value at a given offset.
+     *
+     * @param string $offset
+     * @param mixed $value
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->container[$offset] = $value;
+    }
+
+    /**
+     * Unset the value at a given offset.
+     *
+     * @param string $key
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->container[$offset]);
     }
 }
